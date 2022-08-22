7f3da8abdbdafd15f2979a3f6142e100ab569d8a
Avadhoot Pujari <aditya.avadhoot-pujari@capgemini.com>
2022-08-22 13:42:11 +0530

    first commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,296 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "fa42245f-29dd-4c50-8bad-1f0a2e00b754",
+    "feature": "Search functionality",
+    "scenario": "Check if User is able to click on suggestions by Food Items",
+    "start": 1661151941068,
+    "group": 1,
+    "content": "",
+    "tags": "@search,",
+    "end": 1661151954567,
+    "className": "passed"
+  },
+  {
+    "id": "749faa95-a28d-489a-bf73-ab444be4cc47",
+    "feature": "Swiggy Filters on HomePage Functionality",
+    "scenario": "Check if User is able to clear applied filters",
+    "start": 1661152165977,
+    "group": 1,
+    "content": "",
+    "tags": "@filters,",
+    "end": 1661152188491,
+    "className": "passed"
+  },
+  {
+    "id": "e20f71d6-6ae5-43d4-a48e-42e29bb6e194",
+    "feature": "Location Functionality",
+    "scenario": "Check if the Delivery Location field is giving suggestions for valid city names",
+    "start": 1661151867052,
+    "group": 1,
+    "content": "",
+    "tags": "@location,",
+    "end": 1661151873900,
+    "className": "passed"
+  },
+  {
+    "id": "50b0ab66-a675-46e3-bc0f-fcedef839943",
+    "feature": "Add To Cart functionality",
+    "scenario": "Check if User is able to add multiple food items from same restaurant",
+    "start": 1661152030347,
+    "group": 1,
+    "content": "",
+    "tags": "@addtocart,",
+    "end": 1661152075140,
+    "className": "failed"
+  },
+  {
+    "id": "2a3b9256-cd22-42d8-9b19-8fdeae0bc6b0",
+    "feature": "Swiggy Offers functionality",
+    "scenario": "Check if User is able to get Payment Offers/Coupons",
+    "start": 1661152224041,
+    "group": 1,
+    "content": "",
+    "tags": "@offers,",
+    "end": 1661152237402,
+    "className": "passed"
+  },
+  {
+    "id": "493d55ca-16c4-44b8-920d-7d82c43df0db",
+    "feature": "Swiggy Offers functionality",
+    "scenario": "Check if User is able to copy the Coupon code",
+    "start": 1661152255333,
+    "group": 1,
+    "content": "",
+    "tags": "@offers,",
+    "end": 1661152273071,
+    "className": "passed"
+  },
+  {
+    "id": "404c7233-c420-4c50-9d38-99362ed0494f",
+    "feature": "Location Functionality",
+    "scenario": "Validate the url of Swiggy Page",
+    "start": 1661151856181,
+    "group": 1,
+    "content": "",
+    "tags": "@location,",
+    "end": 1661151860551,
+    "className": "passed"
+  },
+  {
+    "id": "f869148f-cb10-4cc2-8aff-6c1e3c76e8de",
+    "feature": "Search functionality",
+    "scenario": "Check if User is able to click on Popular Cuisines and get suggestions",
+    "start": 1661151954581,
+    "group": 1,
+    "content": "",
+    "tags": "@search,",
+    "end": 1661151972112,
+    "className": "passed"
+  },
+  {
+    "id": "b80f2c1a-ca88-47ca-8b00-81ece48297f8",
+    "feature": "Add To Cart functionality",
+    "scenario": "Check if User is able to view the total cost and discount in Cart page",
+    "start": 1661152075154,
+    "group": 1,
+    "content": "",
+    "tags": "@addtocart,",
+    "end": 1661152119862,
+    "className": "failed"
+  },
+  {
+    "id": "33ca4e13-63da-441c-87b4-5b29ee54ac78",
+    "feature": "Location Functionality",
+    "scenario": "Check if User is able to change the location on Home Page",
+    "start": 1661151897791,
+    "group": 1,
+    "content": "",
+    "tags": "@location,",
+    "end": 1661151907567,
+    "className": "passed"
+  },
+  {
+    "id": "be273314-f68c-4500-9485-c479693e5bdc",
+    "feature": "Search functionality",
+    "scenario": "Check if User is able to click on suggestions by Restaurant name",
+    "start": 1661151927120,
+    "group": 1,
+    "content": "",
+    "tags": "@search,",
+    "end": 1661151941056,
+    "className": "passed"
+  },
+  {
+    "id": "38e3aac8-e105-4f19-9c03-e6b701efe300",
+    "feature": "Add To Cart functionality",
+    "scenario": "Check the behaviour of Cart button when no food item is added",
+    "start": 1661152008105,
+    "group": 1,
+    "content": "",
+    "tags": "@addtocart,",
+    "end": 1661152017864,
+    "className": "passed"
+  },
+  {
+    "id": "59db243b-bb2d-4cb4-a6d1-6abd26bb2c99",
+    "feature": "Add To Cart functionality",
+    "scenario": "Check if User is able to view the list of all Recommended food items",
+    "start": 1661152017875,
+    "group": 1,
+    "content": "",
+    "tags": "@addtocart,",
+    "end": 1661152030336,
+    "className": "passed"
+  },
+  {
+    "id": "5544ea0e-413c-469a-a441-d202bcb4218e",
+    "feature": "Search functionality",
+    "scenario": "Check if User is able to sort the search results by different methods",
+    "start": 1661151989429,
+    "group": 1,
+    "content": "",
+    "tags": "@search,",
+    "end": 1661152008097,
+    "className": "passed"
+  },
+  {
+    "id": "e742ecc9-d9b2-4648-8b08-bdb573b92526",
+    "feature": "Location Functionality",
+    "scenario": "Check if the Delivery Location field is giving suggestions for valid city names",
+    "start": 1661151860612,
+    "group": 1,
+    "content": "",
+    "tags": "@location,",
+    "end": 1661151867035,
+    "className": "passed"
+  },
+  {
+    "id": "be7c67a0-1dd6-434e-a09d-336cd7195a09",
+    "feature": "Search dishes from Restaurant and Add to Cart",
+    "scenario": "Check if User is able to read data from Excel",
+    "start": 1661152119872,
+    "group": 1,
+    "content": "",
+    "tags": "@dishesfromexcel,",
+    "end": 1661152150303,
+    "className": "passed"
+  },
+  {
+    "id": "8f251963-9e65-4b46-9228-39dc21833791",
+    "feature": "Swiggy Offers functionality",
+    "scenario": "Check if User is able to get Restaurant offers",
+    "start": 1661152211733,
+    "group": 1,
+    "content": "",
+    "tags": "@offers,",
+    "end": 1661152224033,
+    "className": "passed"
+  },
+  {
+    "id": "16ee3ad6-d4e4-4dd2-910a-0dbe6d8fea77",
+    "feature": "Location Functionality",
+    "scenario": "Check if the Delivery Location field is giving suggestions for Invalid city names",
+    "start": 1661151873920,
+    "group": 1,
+    "content": "",
+    "tags": "@location,",
+    "end": 1661151879700,
+    "className": "passed"
+  },
+  {
+    "id": "e823f7f2-e407-4c1b-b765-732014605176",
+    "feature": "Swiggy Help functionality",
+    "scenario": "Check if User is able to access Help Page to clarify basic doubts",
+    "start": 1661152188503,
+    "group": 1,
+    "content": "",
+    "tags": "@help,",
+    "end": 1661152211721,
+    "className": "passed"
+  },
+  {
+    "id": "1f80b2a1-befd-46b7-bbb0-a8fd5fa647cd",
+    "feature": "Search functionality",
+    "scenario": "Check if Restaurants and Dishes section are giving results",
+    "start": 1661151972121,
+    "group": 1,
+    "content": "",
+    "tags": "@search,",
+    "end": 1661151989421,
+    "className": "passed"
+  },
+  {
+    "id": "5920a73e-2189-4e78-8bcd-b6c9e7e7148e",
+    "feature": "Swiggy Filters on HomePage Functionality",
+    "scenario": "Check if User is able to click on Cuisines checkboxes and get Restaurants accordingly",
+    "start": 1661152150312,
+    "group": 1,
+    "content": "",
+    "tags": "@filters,",
+    "end": 1661152165967,
+    "className": "passed"
+  },
+  {
+    "id": "94920f5d-4b40-4db9-97e7-e1c50b80aa0e",
+    "feature": "Swiggy Offers functionality",
+    "scenario": "Check if User is able to click on More in Coupons and get the terms and conditions of the Coupon code",
+    "start": 1661152237411,
+    "group": 1,
+    "content": "",
+    "tags": "@offers,",
+    "end": 1661152255315,
+    "className": "passed"
+  },
+  {
+    "id": "6c4a9817-c500-4eb8-a78b-0c1153b8ba51",
+    "feature": "Search functionality",
+    "scenario": "Check if User is able to get suggestions for Restaurant names in Search field",
+    "start": 1661151907581,
+    "group": 1,
+    "content": "",
+    "tags": "@search,",
+    "end": 1661151920118,
+    "className": "passed"
+  },
+  {
+    "id": "5437e040-56a0-4182-9bb8-de2f7de3cef6",
+    "feature": "Location Functionality",
+    "scenario": "Check if clicking on Locate Me button fetch the current location.",
+    "start": 1661151886411,
+    "group": 1,
+    "content": "",
+    "tags": "@location,",
+    "end": 1661151897779,
+    "className": "passed"
+  },
+  {
+    "id": "27925594-26a6-4d3e-85e6-b01a856673e5",
+    "feature": "Location Functionality",
+    "scenario": "Check if User is able to enter other location after clicking on Clear button",
+    "start": 1661151879712,
+    "group": 1,
+    "content": "",
+    "tags": "@location,",
+    "end": 1661151886392,
+    "className": "passed"
+  },
+  {
+    "id": "39ee1ddb-5dd4-48ee-b270-e97cb1f146c2",
+    "feature": "Search functionality",
+    "scenario": "Check if User is able to get suggestion for Invalid restaurant and food item name",
+    "start": 1661151920129,
+    "group": 1,
+    "content": "",
+    "tags": "@search,",
+    "end": 1661151927107,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
